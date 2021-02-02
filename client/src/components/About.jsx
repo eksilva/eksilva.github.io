fbd167360bc1e4a9914601a07403b49ae1605824
Kimo Silva <e.k.silva441@gmail.com>
2021-02-02 14:59:47 -0800

    styling changes and hrefs added

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const About = () => {
   return (
     <div id="about-wrapper">
-      <div id="hi">Hi, my name is</div>
-      <div className="who-i-am">Kimo Silva</div>
-      <span className="who-i-am" id="job-title" >Full Stack Developer<span className="who-i-am" id="come-on-guy">(Freshly-baked!)</span></span>
-      <div id="about-paragraph">I'm a budding software engineer based out wherever you're hiring (i.e. remote). I specialize in minimal complaining and whatever needs to get done.</div>
+      <div id="greeting">Hi, my name is</div>
+      <div className="my-name">Kimo Silva.</div>
+      <span className="my-name" id="job-title" >I'm a Full Stack Developer<span className="my-name" id="come-on-guy">(freshly-baked, too).</span></span>
+      <div id="about-paragraph">Budding software engineer based out wherever you're hiring (i.e. remote), and I specialize in minimal complaining + whatever needs to get done.</div>
     </div>
   )
 }
--- client/src/components/Footing.jsx
+++ client/src/components/Footing.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 import { GrReactjs, GrNode } from "react-icons/gr";
 import { SiWebpack, SiBabel } from 'react-icons/si'
 
+
 const Footing = () => {
   return (
     <div id="footing-wrapper">
       <div className="footing-icons">This page was built using mainly: [</div>
-      <GrReactjs className="footing-icons"/>
-      <SiWebpack className="footing-icons"/>
-      <SiBabel className="footing-icons"/>
-      <GrNode className="footing-icons"/>
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer noopener"><GrReactjs className="footing-icons"/></a>
+        <a href="https://webpack.js.org/" target="_blank" rel="noreferrer noopener"><SiWebpack className="footing-icons"/></a>
+        <a href="https://babeljs.io/" target="_blank" rel="noreferrer noopener"><SiBabel className="footing-icons"/></a>
+        <a href="https://nodejs.org/" target="_blank" rel="noreferrer noopener"><GrNode className="footing-icons"/></a>
       <div>]</div>
     </div>
   )
