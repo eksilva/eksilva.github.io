c7590a379e9f9bcad81e669fb99f06c2787d6ea8
Kimo Silva <e.k.silva441@gmail.com>
2021-02-18 17:20:57 -0800

    footing styling changes

--- client/src/components/Footing.jsx
+++ client/src/components/Footing.jsx
@@ -7,12 +7,11 @@ import { SiWebpack, SiBabel } from 'react-icons/si'
 const Footing = () => {
   return (
     <footer id="footing-wrapper">
-      <div className="footing-icons">This page was built using mainly: [</div>
+      <div className="footing-icons">This page was built using mainly: </div>
         <a href="https://reactjs.org/" target="_blank" rel="noreferrer noopener"><GrReactjs className="footing-icons"/></a>
         <a href="https://webpack.js.org/" target="_blank" rel="noreferrer noopener"><SiWebpack className="footing-icons"/></a>
         <a href="https://babeljs.io/" target="_blank" rel="noreferrer noopener"><SiBabel className="footing-icons"/></a>
         <a href="https://nodejs.org/" target="_blank" rel="noreferrer noopener"><GrNode className="footing-icons"/></a>
-      <div>]</div>
     </footer>
   )
 }
