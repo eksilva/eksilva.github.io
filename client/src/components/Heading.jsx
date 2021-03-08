a8d9d0d2cbddbfe085a746b123885120400f6b58
Kimo Silva <e.k.silva441@gmail.com>
2021-03-08 15:31:35 -0800

    Added a small border radius to images in project frames. Changed out anchor icon to orbital animation and moved it to the right a bit on desktop and mobile.

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -8,7 +8,8 @@ import { SiLinkedin } from "react-icons/si";
 const Heading = () => {
   return (
     <div id="heading-wrapper">
-      <GiAnchor id="heading-icon" />
+      <div className="jawn"></div>
+      {/* <GiAnchor id="heading-icon" /> */}
       <div id="heading-right">
         <a href="https://github.com/eksilva" target="_blank" rel="noreferrer noopener"><GoMarkGithub className="header-link-icons" /></a>
         <a href="https://www.linkedin.com/in/erickimosilva/" target="_blank" rel="noreferrer noopener"><SiLinkedin className="header-link-icons" /></a>
--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,15 +1,23 @@
+/* 
+
+  The individual project component, containing:
+  -Screenshot (Frame)
+  -Project Description
+  -Link to view the github repo
+
+*/
 import React from 'react';
 
-const Project = (props) => {
+const Project = ({ projectInfo }) => {
   return (
     <div className="project-wrapper">
-      <a href={props.projectInfo.heroku} target="_blank" rel="noopener noreferrer">
+      <a href={projectInfo.heroku} target="_blank" rel="noopener noreferrer">
         <div className="project-frame">
-        <img className="project-img" src={props.projectInfo.image} alt={props.projectInfo.name}/>
+        <img className="project-img" src={projectInfo.image} alt={projectInfo.name}/>
         </div>
       </a>
-      <div className="project-name">{props.projectInfo.name}</div>
-      <div className="project-caption">{props.projectInfo.caption} <a className="view-code-link" href={props.projectInfo.github}>View Code</a></div>
+      <div className="project-name">{projectInfo.name}</div>
+      <div className="project-caption">{projectInfo.caption} <a className="view-code-link" href={projectInfo.github} target="_blank" rel="noopener noreferrer">View Code</a></div>
     </div>
   )
 }
